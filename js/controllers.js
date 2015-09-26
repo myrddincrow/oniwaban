488c5ae4551bb7890479c26f240407164d136d51
Samson Lancaster <myrddincrow@gmail.com>
2015-09-25 18:00:46 -0700

    EXPORT!!!!

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,6 @@
 var myApp = angular.module('myApp', [])
 
+
 .filter('noPeriod', function(){
       return function(val){
   var filteredString = val.replace(/\./g, '@2E');
@@ -63,27 +64,90 @@ function corpCheck(corpList, corpName){
 };
 //Loop through Alliances and remove dupes
 angular.forEach($scope.kills, function(key,value){
-  if ($scope.filteredList.allies.lastIndexOf(key.victim.allianceName) === -1 && key.victim.allianceName != ""){
-    $scope.filteredList.allies.push(key.victim.allianceName);
+  if (!allyCheck($scope.filteredList.allies, key.victim.allianceName) && key.victim.allianceName != ""){
+    $scope.filteredList.allies.push({allyName:key.victim.allianceName});
   }
     angular.forEach(key.attackers, function(key,attackerValue){
-      if ($scope.filteredList.allies.lastIndexOf(key.allianceName) === -1 && key.allianceName != ""){
-        $scope.filteredList.allies.push(key.allianceName);
+      if (!allyCheck($scope.filteredList.allies, key.allianceName) && key.allianceName != ""){
+        $scope.filteredList.allies.push({allyName:key.allianceName});
       }
     })
 });
+
+function allyCheck(allyList, allyName){
+  for (var i = 0; i < allyList.length; i++){
+    if (allyList[i].allyName == allyName){
+      return true;
+      break
+    }
+  }
+  return false;
+};
+
 
 angular.forEach($scope.kills, function(key,value){
-  if ($scope.filteredList.ships.lastIndexOf(key.victim.shipTypeID) === -1 && key.victim.shipTypeID != ""){
-    $scope.filteredList.ships.push(key.victim.shipTypeID);
+  if (!shipCheck($scope.filteredList.ships, key.victim.shipTypeID) && key.victim.shipTypeID != ""){
+    $scope.filteredList.ships.push({shipType:key.victim.shipTypeID});
   }
     angular.forEach(key.attackers, function(key,attackerValue){
-      if ($scope.filteredList.ships.lastIndexOf(key.shipTypeID) === -1 && key.shipTypeID != ""){
-        $scope.filteredList.ships.push(key.shipTypeID);
+      if (!shipCheck($scope.filteredList.ships, key.shipTypeID) && key.shipTypeID != ""){
+        $scope.filteredList.ships.push({shipType:key.shipTypeID});
       }
     })
 });
 
+function shipCheck(shipList, shipType){
+  for (var i = 0; i < shipList.length; i++){
+    if (shipList[i].shipType == shipType){
+      return true;
+      break
+    }
+  }
+  return false;
+};
+
+function dataChecker(index,target,value){
+try{
+  return $scope.filteredList[target][index][value];
+}catch(e){
+  return "";
+}
+}
+
+$scope.exportData =function(){
+var csvData = [];
+
+                      csvData.push("Pilots, Corps, Alliances, Ships,")
+
+                      //////YOUR ARRAY
+                      ///REPLACE -  result.data with your stuff
+for (var i = 0; i < $scope.filteredList.pilots.length; i++){
+  csvData.push($scope.filteredList.pilots[i].pilotName + "," + dataChecker( i, "corps", "corpName") + "," + dataChecker( i, "allies", "allyName") + "," + dataChecker( i, "ships", "shipType") + ",");
+}
+
+
+                      var fileName = "OniExport.csv";
+                      var buffer = csvData.join("\n");
+                      var blob = new Blob([buffer], {
+                          "type": "text/csv;charset=utf8;"
+                      });
+
+                      var link = document.createElement("a");
+
+                      if(link.download !== undefined) {
+                          ////HTML 5 browsers only
+                          link.setAttribute("href", window.URL.createObjectURL(blob));
+                          link.setAttribute("download", fileName);
+                      }
+                      else if(navigator.msSaveBlob) {
+                          // IE 10+ special handler
+                          navigator.msSaveBlob(blob, fileName);
+                      }
+
+                      document.body.appendChild(link);
+
+                      link.click();
+}
 
 });
 }]);
